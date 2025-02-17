adc6775b6e5f7928f1f9ab579421ecbd1e207cec
Geon-o <shjwin96@gmail.com>
2025-02-17 17:26:49 +0900

    feat: 사용자가 회원가입할 수 있는 페이지 UI [1][미완료]

---
+++ trip_planner_front/src/pages/user/sign_up/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+const index = () => {
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.container__title}> 환영합니다! 🎉</h2>
+        </div>
+    )
+}
+
+export default index
