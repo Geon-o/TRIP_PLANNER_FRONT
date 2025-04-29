103277c9a0081966c20c31e3c12cf1f9b14d2961
Geon-o <shjwin96@gmail.com>
2025-04-30 08:42:04 +0900

    원복

--- trip_planner_front/src/pages/user/sign_in/index.tsx
+++ trip_planner_front/src/pages/user/sign_in/index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
     return (
         <div className={styles.container}>
             <h2 className={styles.container__title}>
-                테스트
+                로그인
             </h2>
             <Stack width={"100%"}>
                 <InputGroup>
