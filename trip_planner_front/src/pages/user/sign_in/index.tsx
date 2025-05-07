d58786facd5dba304e8c5263d915ab34d3cebc60
Geon-o <shjwin96@gmail.com>
2025-05-07 11:41:45 +0900

    chore: 로그인/회원가입 백그라운드 이미지 적용

--- trip_planner_front/src/pages/user/sign_in/index.tsx
+++ trip_planner_front/src/pages/user/sign_in/index.tsx
@@ -1,5 +1,5 @@
 import styles from './index.module.scss';
-import {Button, Flex, IconButton, Input, InputGroup, InputRightElement, Stack, Text} from "@chakra-ui/react";
+import {Box, Button, Flex, IconButton, Input, InputGroup, InputRightElement, Stack, Text} from "@chakra-ui/react";
 import * as React from "react";
 import {FaRegEye, FaRegEyeSlash} from "react-icons/fa";
 import {useEffect} from "react";
@@ -9,6 +9,7 @@ import {useSignInHandle} from "@pages/user/sign_in/hook/useSignInHandle.tsx";
 import useSignInFormStore from "@pages/user/sign_in/store/useSignInFormStore.tsx";
 import {apiSignIn} from "@pages/user/sign_in/service/signInService.api.tsx";
 import {useNavigate} from "react-router-dom";
+import loginBackground from '@/assets/trip_background.png';
 
 const Index = () => {
     const [isShowPassword, setIsShowPassword] = React.useState<boolean>(false);
@@ -65,48 +66,74 @@ const Index = () => {
     }, [handleId, handlePassword]);
 
     return (
-        <div className={styles.container}>
-            <h2 className={styles.container__title}>
-                로그인
-            </h2>
-            <Stack width={"100%"}>
-                <InputGroup>
-                    <Input type={"text"}
-                           placeholder="아이디"
-                           onChange={handleId}
-                           isInvalid
-                           errorBorderColor={!isId ? 'none' : 'red.300'}
-                    />
-                </InputGroup>
-                <Text fontSize='10px'>{idMessage}</Text>
+        <Box bgImage={`url(${loginBackground})`}
+             bgSize="cover"
+             bgPosition="center"
+             bgRepeat="no-repeat"
+             minH="100vh"
+             minW="100vw"
+             display="flex"
+             justifyContent="center"
+             alignItems="center">
+            <Box
+                bg="rgba(255, 255, 255, 0.3)" // 반투명 흰 배경
+                backdropFilter="blur(3px)"    // 배경 흐림 효과
+                height="100vh"
+                width="100vw"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Box justifyContent="center"
+                     alignItems="center"
+                     display="flex"
+                     flexDirection="column"
+                >
+                    <div className={styles.container}>
+                        <h2 className={styles.container__title}>
+                            로그인
+                        </h2>
+                        <Stack width={"100%"}>
+                            <InputGroup>
+                                <Input type={"text"}
+                                       placeholder="아이디"
+                                       onChange={handleId}
+                                       isInvalid
+                                       errorBorderColor={!isId ? 'none' : 'red.300'}
+                                />
+                            </InputGroup>
+                            <Text fontSize='10px'>{idMessage}</Text>
 
-                <InputGroup>
-                    <Input type={isShowPassword ? 'text' : "password"}
-                           placeholder="비밀번호"
-                           onChange={handlePassword}
-                           isInvalid
-                           errorBorderColor={!isPassword ? 'none' : 'red.300'}
-                    />
-                    <InputRightElement width='3rem'>
-                        <IconButton h='1.75rem'
-                                    icon={isShowPassword ? <FaRegEyeSlash/> : <FaRegEye/>}
-                                    onClick={() => setIsShowPassword(!isShowPassword)}/>
-                    </InputRightElement>
-                </InputGroup>
-                <Text fontSize='10px'>{passwordMessage}</Text>
+                            <InputGroup>
+                                <Input type={isShowPassword ? 'text' : "password"}
+                                       placeholder="비밀번호"
+                                       onChange={handlePassword}
+                                       isInvalid
+                                       errorBorderColor={!isPassword ? 'none' : 'red.300'}
+                                />
+                                <InputRightElement width='3rem'>
+                                    <IconButton h='1.75rem'
+                                                icon={isShowPassword ? <FaRegEyeSlash/> : <FaRegEye/>}
+                                                onClick={() => setIsShowPassword(!isShowPassword)}/>
+                                </InputRightElement>
+                            </InputGroup>
+                            <Text fontSize='10px'>{passwordMessage}</Text>
 
-                <Flex justifyContent='center' mt={6} mb={3}>
-                    <Button width='300px'
-                            onClick={signIn}
-                    >
-                        로그인
-                    </Button>
-                </Flex>
-                <Flex>
-                    <Text fontSize={'13px'} >아직 회원이 아니신가요? <a href={'/signUp'} style={{fontWeight: 'bolder', color: 'indianred'}}>회원가입</a>을 해보세요. :)</Text>
-                </Flex>
-            </Stack>
-        </div>
+                            <Flex justifyContent='center' mt={6} mb={3}>
+                                <Button width='300px'
+                                        onClick={signIn}
+                                >
+                                    로그인
+                                </Button>
+                            </Flex>
+                            <Flex>
+                                <Text fontSize={'13px'} >아직 회원이 아니신가요? <a href={'/signUp'} style={{fontWeight: 'bolder', color: 'indianred'}}>회원가입</a>을 해보세요. :)</Text>
+                            </Flex>
+                        </Stack>
+                    </div>
+                </Box>
+            </Box>
+        </Box>
     );
 };
 
--- trip_planner_front/src/pages/user/sign_up/index.tsx
+++ trip_planner_front/src/pages/user/sign_up/index.tsx
@@ -6,12 +6,13 @@ import * as React from 'react';
 import {useEffect, useRef} from 'react';
 import styles from './index.module.scss';
 import {FaRegEye, FaRegEyeSlash} from "react-icons/fa";
-import {Button, Flex, IconButton, Input, InputGroup, InputRightElement, Stack, Text} from '@chakra-ui/react';
+import {Box, Button, Flex, IconButton, Input, InputGroup, InputRightElement, Stack, Text} from '@chakra-ui/react';
 import Timer from "@pages/user/sign_up/utils/Timer.tsx";
 import useSignUpStore from "@pages/user/sign_up/store/useSignUpStore.tsx";
 import {useSignFormValid} from "@pages/user/sign_up/hook/useSignFormValid.tsx";
 import {useSignUpDataVerification} from "@pages/user/sign_up/hook/useSignUpDataVerification.tsx";
 import useSignUpFormStore from "@pages/user/sign_up/store/useSignUpFormStore.tsx";
+import loginBackground from "@assets/trip_background.png";
 
 const index = () => {
     // ==================== store =====================
@@ -56,124 +57,148 @@ const index = () => {
     }, [handleId, handleEmail, handleEmailConf, handlePassword, handlePasswordCheck]);
 
     return (
-        <div className={styles.container}>
-            <h2 className={styles.container__title}>회원가입</h2>
-            <Stack width='100%'>
-                <InputGroup>
-                    <Input type="email"
-                           placeholder="이메일"
-                           isInvalid
-                           disabled={disabledEmailAuthForm}
-                           errorBorderColor={!isEmail ? 'none' : 'red.300'}
-                           onChange={handleEmail}/>
-                    <InputRightElement width={showEmailConf ? '4.2rem' : '3.5rem'}>
-                        <Button size='sm'
-                                h='1.75rem'
-                                variant='outline'
-                                fontSize='12px'
-                                onClick={checkEmail}
-                                disabled={disabledEmailConfBtn || disabledEmailAuthForm}
-                                name="emailConfBtn"
-                        >
-                            {emailTokenAuthBtnName}
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-                <Text fontSize='10px'>{emailMessage}</Text>
-
-                {
-                    showEmailConf &&
-                    <Stack>
-                        <InputGroup>
-                            <Input type="text"
-                                   placeholder="인증번호"
-                                   isInvalid
-                                   errorBorderColor={!isEmailConf ? 'none' : 'red.300'}
-                                   onChange={handleEmailConf}
-                                   disabled={disabledEmailAuthForm}
-                            />
-                            <InputRightElement width='8.5rem'>
-                                <Text fontSize='10px' h='1rem'>
-                                    <Timer onReset={(resetFn) => (resetTimerRef.current = resetFn)}
-                                           disabled={disabledEmailAuthForm}/>
-                                </Text>
-                            </InputRightElement>
-                            <InputRightElement width='3.5rem'>
-                                <Button size='sm'
-                                        h='1.75rem'
-                                        variant='outline'
-                                        fontSize='12px'
-                                        disabled={disabledDetailInfoBtn || disabledEmailAuthForm}
-                                        onClick={checkAuthToken}
-                                >
-                                    확인
-                                </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                        <Text fontSize='10px'>{emailConfMessage}</Text>
-                    </Stack>
-                }
-
-                {
-                    showDetailInfo &&
-                    <div>
-                        <Stack>
+        <Box bgImage={`url(${loginBackground})`}
+             bgSize="cover"
+             bgPosition="center"
+             bgRepeat="no-repeat"
+             minH="100vh"
+             minW="100vw"
+             display="flex"
+             justifyContent="center"
+             alignItems="center">
+            <Box
+                bg="rgba(255, 255, 255, 0.3)" // 반투명 흰 배경
+                backdropFilter="blur(3px)"    // 배경 흐림 효과
+                height="100vh"
+                width="100vw"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Box justifyContent="center"
+                     alignItems="center"
+                     display="flex"
+                     flexDirection="column"
+                >
+                    <div className={styles.container}>
+                        <h2 className={styles.container__title}>회원가입</h2>
+                        <Stack width='100%'>
                             <InputGroup>
-                                <Input type="text"
-                                       placeholder="아이디"
+                                <Input type="email"
+                                       placeholder="이메일"
                                        isInvalid
-                                       errorBorderColor={!isId ? 'none' : 'red.300'}
-                                       onChange={handleId}
-                                       size='md'
-                                />
-                                <InputRightElement width='4.9rem'>
+                                       disabled={disabledEmailAuthForm}
+                                       errorBorderColor={!isEmail ? 'none' : 'red.300'}
+                                       onChange={handleEmail}/>
+                                <InputRightElement width={showEmailConf ? '4.2rem' : '3.5rem'}>
                                     <Button size='sm'
                                             h='1.75rem'
                                             variant='outline'
                                             fontSize='12px'
-                                            onClick={checkDuplicateId}
+                                            onClick={checkEmail}
+                                            disabled={disabledEmailConfBtn || disabledEmailAuthForm}
+                                            name="emailConfBtn"
                                     >
-                                        중복확인
+                                        {emailTokenAuthBtnName}
                                     </Button>
                                 </InputRightElement>
                             </InputGroup>
-                            <Text fontSize='10px'>{idMessage}</Text>
+                            <Text fontSize='10px'>{emailMessage}</Text>
 
-                            <InputGroup>
-                                <Input type={showPasswordText ? 'text' : "password"}
-                                       placeholder="비밀번호"
-                                       isInvalid
-                                       errorBorderColor={!isPassword ? 'none' : 'red.300'}
-                                       onChange={handlePassword}/>
+                            {
+                                showEmailConf &&
+                                <Stack>
+                                    <InputGroup>
+                                        <Input type="text"
+                                               placeholder="인증번호"
+                                               isInvalid
+                                               errorBorderColor={!isEmailConf ? 'none' : 'red.300'}
+                                               onChange={handleEmailConf}
+                                               disabled={disabledEmailAuthForm}
+                                        />
+                                        <InputRightElement width='8.5rem'>
+                                            <Text fontSize='10px' h='1rem'>
+                                                <Timer onReset={(resetFn) => (resetTimerRef.current = resetFn)}
+                                                       disabled={disabledEmailAuthForm}/>
+                                            </Text>
+                                        </InputRightElement>
+                                        <InputRightElement width='3.5rem'>
+                                            <Button size='sm'
+                                                    h='1.75rem'
+                                                    variant='outline'
+                                                    fontSize='12px'
+                                                    disabled={disabledDetailInfoBtn || disabledEmailAuthForm}
+                                                    onClick={checkAuthToken}
+                                            >
+                                                확인
+                                            </Button>
+                                        </InputRightElement>
+                                    </InputGroup>
+                                    <Text fontSize='10px'>{emailConfMessage}</Text>
+                                </Stack>
+                            }
 
-                                <InputRightElement width='3rem'>
-                                    <IconButton h='1.75rem'
-                                                icon={showPasswordText ? <FaRegEyeSlash/> : <FaRegEye/>}
-                                                onClick={() => setShowPasswordText(!showPasswordText)}/>
-                                </InputRightElement>
-                            </InputGroup>
-                            <Text fontSize='10px'>{passwordMessage}</Text>
+                            {
+                                showDetailInfo &&
+                                <div>
+                                    <Stack>
+                                        <InputGroup>
+                                            <Input type="text"
+                                                   placeholder="아이디"
+                                                   isInvalid
+                                                   errorBorderColor={!isId ? 'none' : 'red.300'}
+                                                   onChange={handleId}
+                                                   size='md'
+                                            />
+                                            <InputRightElement width='4.9rem'>
+                                                <Button size='sm'
+                                                        h='1.75rem'
+                                                        variant='outline'
+                                                        fontSize='12px'
+                                                        onClick={checkDuplicateId}
+                                                >
+                                                    중복확인
+                                                </Button>
+                                            </InputRightElement>
+                                        </InputGroup>
+                                        <Text fontSize='10px'>{idMessage}</Text>
 
-                            <Input type="password"
-                                   placeholder="비밀번호 확인"
-                                   isInvalid
-                                   errorBorderColor={!isPasswordCheck ? 'none' : 'red.300'}
-                                   onChange={handlePasswordCheck}/>
-                            <Text fontSize='10px'>{passwordCheckMessage}</Text>
+                                        <InputGroup>
+                                            <Input type={showPasswordText ? 'text' : "password"}
+                                                   placeholder="비밀번호"
+                                                   isInvalid
+                                                   errorBorderColor={!isPassword ? 'none' : 'red.300'}
+                                                   onChange={handlePassword}/>
+
+                                            <InputRightElement width='3rem'>
+                                                <IconButton h='1.75rem'
+                                                            icon={showPasswordText ? <FaRegEyeSlash/> : <FaRegEye/>}
+                                                            onClick={() => setShowPasswordText(!showPasswordText)}/>
+                                            </InputRightElement>
+                                        </InputGroup>
+                                        <Text fontSize='10px'>{passwordMessage}</Text>
+
+                                        <Input type="password"
+                                               placeholder="비밀번호 확인"
+                                               isInvalid
+                                               errorBorderColor={!isPasswordCheck ? 'none' : 'red.300'}
+                                               onChange={handlePasswordCheck}/>
+                                        <Text fontSize='10px'>{passwordCheckMessage}</Text>
+                                    </Stack>
+                                    <Flex justifyContent='center' mt={10} mb={10}>
+                                        <Button width='300px'
+                                                onClick={signUp}
+                                        >
+                                            회원가입
+                                        </Button>
+                                    </Flex>
+                                </div>
+                            }
                         </Stack>
-                        <Flex justifyContent='center' mt={10} mb={10}>
-                            <Button width='300px'
-                                    onClick={signUp}
-                            >
-                                회원가입
-                            </Button>
-                        </Flex>
                     </div>
-                }
-            </Stack>
-
-
-        </div>
+                </Box>
+            </Box>
+        </Box>
     );
 }
 
