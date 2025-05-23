d143005cb7acad26f0787793d2ffed935fd65c5d
Geon-o <shjwin96@gmail.com>
2025-05-23 14:54:50 +0900

    chore: 서버로부터 전달받은 accessToken api 요청시 전달하도록 수정

--- trip_planner_front/src/config/Api.ts
+++ trip_planner_front/src/config/Api.ts
@@ -1,11 +1,26 @@
 import axios, {AxiosInstance} from "axios";
+import {useAuthStore} from "@pages/user/sign_in/store/useAuthStore.tsx";
 
 const Api: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true
+  // baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL,
+  baseURL: 'http://localhost:9999',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true
 });
+
+Api.interceptors.request.use((config) => {
+    const token = useAuthStore.getState().accessToken;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 export default Api;
