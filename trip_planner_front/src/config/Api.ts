0e16bfa2ae41cf029498783fb47513c388f1984c
Geon-o <shjwin96@gmail.com>
2025-05-25 20:49:23 +0900

    chore: Jwt 재발급 로직 추가

--- trip_planner_front/src/config/Api.ts
+++ trip_planner_front/src/config/Api.ts
@@ -1,10 +1,9 @@
 import axios, {AxiosInstance} from "axios";
 import {useAuthStore} from "@pages/user/sign_in/store/useAuthStore.tsx";
-import {persist} from "zustand/middleware";
 
 const Api: AxiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL,
-  baseURL: 'http://localhost:9999',
+  baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL,
+  // baseURL: 'http://localhost:9999',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -24,25 +23,37 @@ Api.interceptors.request.use((config) => {
   }
 );
 
-Api.interceptors.request.use(
-  (res) => res,
-  async (error) => {
-    if (error.response?.status === 401) {
-      try {
-        const res = await axios.post('/auth/reIssuanceToken');
-        console.log(res);
-        const newToken = res.authToken;
+Api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
 
-        useAuthStore.getState().setAccessToken(newToken);
-        useAuthStore.persist.rehydrate();
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
 
-      } catch (error) {
-        alert('세션이 만료됐습니다. \n다시 로그인해주세요.');
-        useAuthStore.getState().clearAccessToken();
-        window.location.href = '/signIn';
-      }
+            try {
+                const res = await Api.post(
+                    '/auth/reIssuanceToken',
+                    null,
+                    { withCredentials: true }
+                );
+
+                const newToken = res.data.newAccessToken;
+                useAuthStore.getState().setAccessToken(newToken);
+                useAuthStore.persist.rehydrate();
+
+                // 새 토큰으로 Authorization 헤더 덮어쓰기
+                originalRequest.headers.Authorization = `Bearer ${newToken}`;
+                return axios(originalRequest); // 요청 재시도
+            } catch (err) {
+                useAuthStore.getState().clearAccessToken();
+                window.location.href = '/signIn';
+                return Promise.reject(err);
+            }
+        }
+
+        return Promise.reject(error);
     }
-  }
-)
+);
 
 export default Api;
